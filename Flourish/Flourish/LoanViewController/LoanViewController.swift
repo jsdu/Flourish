91b6078d9b627113338dfd1fd9d7e89bbabc9ec4
Jason Du <jdu@dynamicsignal.com>
2017-10-21 15:50:12 -0700

    Add data to test ui

--- Flourish/Flourish/LoanViewController/LoanViewController.swift
+++ Flourish/Flourish/LoanViewController/LoanViewController.swift
@@ -11,4 +11,52 @@ import UIKit
 
 class LoanViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    let loanInProgressArr = ["",""]
+    let loanCompleteArr = ["","",""]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+extension LoanViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return loanInProgressArr.count + loanCompleteArr.count + 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LoanDashBoardCell") as! LoanDashBoardCell
+            return cell
+        } else if indexPath.row <= loanInProgressArr.count  {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LoanInProgressCell") as! LoanInProgressCell
+            return cell
+        } else if indexPath.row == loanInProgressArr.count + 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LoanHeaderCell") as! LoanHeaderCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LoanCompleteCell") as! LoanCompleteCell
+            return cell
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 195.0
+        } else if indexPath.row <= loanInProgressArr.count  {
+            return 160.0
+        } else if indexPath.row == loanInProgressArr.count + 1 {
+            return 40.0
+        } else {
+            return 129.0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+        } else if indexPath.row <= loanInProgressArr.count  {
+        }
+    }
 }
